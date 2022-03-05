d30a51645b7a476038b4cf8b37840f11a575192d
pablosgreccia <pablosgreccia@gmail.com>
2022-03-05 19:16:51 -0300

    adding responsive tester

---
+++ js/DOM/responsive_tester.js
@@ -0,0 +1,34 @@
+const d = document;
+const w = window;
+
+export default function responsiveTester(form){
+
+    let $form = d.querySelector(form);
+    let tester;
+
+    $form.addEventListener("submit", (e) => {
+        if (e.target === $form) {
+            e.preventDefault();
+            tester = w.open($form.website.value, "tester", `innerWidth=${$form.width.value}, innerHeight=${$form.height.value}`);
+        }
+    });
+
+    $form.addEventListener("click", (e) => {
+        console.log(e.target);
+        if (e.target === $form.close) {
+            tester.close();
+            $form.website.value = null;
+            $form.height.value = null;
+            $form.width.value = null;
+
+        }
+    });
+
+    // $form.addEventListener("reset", (e) => {
+    //     // e.preventDefault();
+    //     // $form.website.value = null;
+    //     // $form.height.value = null;
+    //     // $form.width.value = null;
+    // });
+
+}
